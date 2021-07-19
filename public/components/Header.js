d13b118b61141cbff15f17cc83552ed7130df09b
Patrick McSweeney <PMcSweeney@bellevuewa.gov>
2021-07-19 10:20:07 -0700

    Included Cheryl's UI update

--- public/components/Header.js
+++ public/components/Header.js
@@ -2,8 +2,7 @@ class Header extends React.Component {
     render() {
         return (
             <header>
-              <h1>City of Bellevue</h1>
-              <h2>Dev team interests:</h2>
+                <h1>ITD Dev/DGov Team Interests</h1>
             </header>
         );
     }
