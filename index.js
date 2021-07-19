105b8421dbabcfcf526c6e1cd6211165ed5b8341
Patrick McSweeney <PMcSweeney@bellevuewa.gov>
2021-07-19 10:15:08 -0700

    Working on snack web

--- index.js
+++ index.js
@@ -13,25 +13,38 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 const DEBUG = true;
 
 var database = {
-    interests: []
+    interests: [],
+    snacks: []
 }
 
 client.connect(err => {
-    const collection = client.db("InterestWeb").collection("interests");
+    const interestCollection = client.db("InterestWeb").collection("interests");
 
-    var interests = collection.find()
+    var interests = interestCollection.find()
     interests.toArray(function(err, docs) {
         docs.forEach(function(doc) {
             console.log(doc);
             database.interests.push(doc);
         });
     });
+
+    const snackCollection = client.db("InterestWeb").collection("snacks");
+
+    var snacks = snackCollection.find();
+    snacks.toArray(function(err, docs) {
+        docs.forEach(function(doc) {
+            console.log(doc);
+            database.snacks.push(doc);
+        });
+    });
+
 });
 
-function subscribeUserToInterest(data, interestIndex) {
+function subscribeUserToDataType(dataType, dataArray, data, interestIndex) {
     var filter = { name: data.value };
     var newData = { $push: { "subscribedUsers": data.username }};
-    const collection = client.db("InterestWeb").collection("interests");
+
+    const collection = getCollection(dataType) 
     
     collection.updateOne(filter, newData, function(err) {
         if (err) {
@@ -39,15 +52,26 @@ function subscribeUserToInterest(data, interestIndex) {
             return;
         }
 
-        database.interests[interestIndex].subscribedUsers.push(data.username);
+        dataArray[interestIndex].subscribedUsers.push(data.username);
         io.emit('data update', database);
     });
 }
 
-function removeUserFromInterest(data, interestIndex) {
+function getCollection(dataType) {
+    switch(dataType) {
+        case 'interests':
+            return client.db("InterestWeb").collection("interests");
+        case 'snacks':
+            return client.db("InterestWeb").collection("snacks");
+        default:
+            return 'invalid';
+    }
+}
+
+function removeUserFromDataType(dataType, dataArray, data, interestIndex) {
     var filter = { name: data.value };
     var removeData = { $pull: {subscribedUsers: data.username }};
-    const collection = client.db("InterestWeb").collection("interests");
+    const collection = getCollection(dataType);
 
     collection.updateOne(filter, removeData, function(err) {
         if (err) {
@@ -55,37 +79,37 @@ function removeUserFromInterest(data, interestIndex) {
             return;
         }
 
-        var usernameIndex = database.interests[interestIndex].subscribedUsers.indexOf(data.username);
+        var usernameIndex = dataArray[interestIndex].subscribedUsers.indexOf(data.username);
         if (usernameIndex > -1)
-            database.interests[interestIndex].subscribedUsers.splice(usernameIndex, 1);
+            dataArray[interestIndex].subscribedUsers.splice(usernameIndex, 1);
         
-        if (database.interests[interestIndex].subscribedUsers.length == 0)
-            removeEmptyInterest(data.value);
+        if (dataArray[interestIndex].subscribedUsers.length == 0)
+            removeEmptyDataType(dataType, dataArray, data.value);
         else
             io.emit('data update', database);
     })
 }
 
-function removeEmptyInterest(interest) {
-    console.log('deleting interest ' + interest);
+function removeEmptyDataType(dataType, dataArray, datum) {
+    console.log('deleting datum ' + datum);
 
-    const collection = client.db('InterestWeb').collection('interests');
-    var query = { name: interest };
+    const collection = getCollection(dataType);
+    var query = { name: datum };
 
     collection.deleteOne(query, function(err, obj) {
         if (err) console.log(err);
         
-        database.interests = database.interests.filter(x => x.name != interest);
+        database[dataType] = dataArray.filter(x => x.name != datum);
         io.emit('data update', database);
     })
 }
 
-function createInterestWithUser(data) {
+function createDataTypeWithUser(dataType, dataArray, data) {
     var newInterest = {
         name: data.value,
         subscribedUsers: [data.username]
     }
-    const collection = client.db("InterestWeb").collection("interests");
+    const collection = getCollection(dataType);
 
     collection.insertOne(newInterest, function(err) {
         if (err) {
@@ -93,12 +117,69 @@ function createInterestWithUser(data) {
             return;
         }
         
-        database.interests.push({
+        dataArray.push({
             name: data.value,
             subscribedUsers: [data.username]    
         });
         io.emit('data update', database);
     });
+}
+
+
+function submitData(dataType, data) {
+    console.log('Type Submitted: ' + dataType);
+    var foundInterest = false;
+    var dataArray = getData(dataType);
+    for (var i = 0; i < dataArray.length; i++) {
+        if (dataArray[i].name.localeCompare(data.value) == 0)
+        {
+            if (!dataArray[i].subscribedUsers.includes(data.username)) {
+                subscribeUserToDataType(dataType, dataArray, data, i);
+            } else {
+                console.log("Data submission already exists");
+                return;
+            }
+            foundInterest = true;
+        }
+    }
+    if (!foundInterest) {
+        console.log('New datum!');
+        createDataTypeWithUser(dataType, dataArray, data);
+    }
+}
+
+function getData(dataType) {
+    switch(dataType) {
+        case 'interests':
+            return database.interests;
+        case 'snacks':
+            return database.snacks;
+        default:
+            return 'invalid';
+    }
+}
+
+function removeData(dataType, data) {
+    console.log("Removing interest " + data.value + " from user " + data.username);
+    
+    var foundInterest = false;
+    var dataArray = getData(dataType);
+    for (var i = 0; i < dataArray.length; i++) {
+        if (dataArray[i].name.localeCompare(data.value) == 0)
+        {
+            if (!dataArray[i].subscribedUsers.includes(data.username)) {
+                console.log("User not subscribed to this interest");
+            } else {
+                removeUserFromDataType(dataType, dataArray, data, i);
+                return;
+            }
+            foundInterest = true;
+        }
+    }
+    if (!foundInterest) {
+        console.log('Interest not found');
+        return;
+    }
 }
 
 app.get('/', (req, res) => {
@@ -111,45 +192,20 @@ io.on('connection', (socket) => {
 
     socket.on('submit interest', data => {
         console.log('Interest Submitted: ' + data.value);
-        var foundInterest = false;
-        for (var i = 0; i < database.interests.length; i++) {
-            if (database.interests[i].name.localeCompare(data.value) == 0)
-            {
-                if (!database.interests[i].subscribedUsers.includes(data.username)) {
-                    subscribeUserToInterest(data, i);
-                } else {
-                    console.log("Interest already noted");
-                    return;
-                }
-                foundInterest = true;
-            }
-        }
-        if (!foundInterest) {
-            console.log('New Interest!');
-            createInterestWithUser(data);
-        }
+        submitData('interests', data);
     });
 
     socket.on('remove interest', data => {
         console.log("Removing interest " + data.value + " from user " + data.username);
-        
-        var foundInterest = false;
-        for (var i = 0; i < database.interests.length; i++) {
-            if (database.interests[i].name.localeCompare(data.value) == 0)
-            {
-                if (!database.interests[i].subscribedUsers.includes(data.username)) {
-                    console.log("User not subscribed to this interest");
-                } else {
-                    removeUserFromInterest(data, i);
-                    return;
-                }
-                foundInterest = true;
-            }
-        }
-        if (!foundInterest) {
-            console.log('Interest not found');
-            return;
-        }
+        removeData('interests', data);
+    })
+
+    socket.on('submit snack', data => {
+
+    })
+
+    socket.on('remove snack', data => {
+
     })
 
     socket.on('disconnect', () => {
